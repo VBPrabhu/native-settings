beedb42fe5fe9e4e8763cbc676f2843e29ca6715
Roger Huser <rogerhuser@172-15-1-219.lightspeed.stlsmo.sbcglobal.net>
2017-04-23 09:38:25 +0200

    Modified settings variable

--- src/ios/CDVSettings.h
+++ src/ios/CDVSettings.h
@@ -2,6 +2,8 @@
 
 @interface CDVSettings : CDVPlugin
 
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
 - (void)openSettings:(CDVInvokedUrlCommand*)command;
 
 @end
